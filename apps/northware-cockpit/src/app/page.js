c34e0b9b991a49b702adac2add039bd907225ee4
onissen <56229919+onissen@users.noreply.github.com>
2024-09-13 16:47:48 +0200

    Now using @northware(tailwind-config package as preset for tailwind.config

--- apps/northware-cockpit/src/app/page.js
+++ apps/northware-cockpit/src/app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-red-100 text-xl font-bold">Northware Cockpit</h1>
+      <h1 className="bg-primary-500 text-onPrimary text-xl font-bold">
+        Northware Cockpit
+      </h1>
+      <h1 className="text-secondary text-xl font-bold">Northware Cockpit</h1>
+      <h1 className="text-cockpit-500 text-xl font-bold">Northware Cockpit</h1>
+      <h1 className="text-finance-500 text-xl font-bold">Northware Finance</h1>
+      <h1 className="text-trader-500 text-xl font-bold">Northware Trader</h1>
     </div>
   );
 }
--- apps/northware-cockpit/tailwind.config.js
+++ apps/northware-cockpit/tailwind.config.js
@@ -1,13 +1,14 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  presets: [require("@northware/tailwind-config")],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: colors.sky,
+        onPrimary: colors.white,
       },
     },
   },
-  plugins: [],
 };
