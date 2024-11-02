d5377eaaff1daf7bdae4a8847b89c1a9c4c57cd3
onissen <56229919+onissen@users.noreply.github.com>
2024-11-02 17:11:34 +0100

    fix(cockpit): Strukturierte Login Page
    Die Login Page implementiert das LoginForm aus @northware/auth. Das Styling des Forms und der Seite drum herum wird später von Theme und UI übernommen.

--- apps/northware-cockpit/src/app/login/page.jsx
+++ apps/northware-cockpit/src/app/login/page.jsx
@@ -1,38 +1,24 @@
-import Link from "next/link";
-import { Form } from "@northware/auth/components";
+import { LoginForm } from "@northware/auth/components";
 import { signIn } from "@northware/auth/auth";
-import { SubmitButton } from "@northware/auth/components";
 
 export default function Login() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
-      <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
-        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-          <h3 className="text-xl font-semibold">Sign In</h3>
-          <p className="text-sm text-gray-500">
-            Use your email and password to sign in
-          </p>
+    <main className={"${layout.login} ${layout.cockpit}"}>
+      <section className={"layout.loginBox"}>
+        <div id="loginForm">
+          <h1 className="mb-4 text-center">Login</h1>
+          <LoginForm
+            action={async (formData) => {
+              "use server";
+              await signIn("credentials", {
+                redirectTo: "/dashboard",
+                email: formData.get("email"),
+                password: formData.get("password"),
+              });
+            }}
+          />
         </div>
-        <Form
-          action={async (formData) => {
-            "use server";
-            await signIn("credentials", {
-              redirectTo: "/dashboard",
-              email: formData.get("email"),
-              password: formData.get("password"),
-            });
-          }}
-        >
-          <SubmitButton>Sign in</SubmitButton>
-          <p className="text-center text-sm text-gray-600">
-            {"Don't have an account? "}
-            <Link href="/register" className="font-semibold text-gray-800">
-              Sign up
-            </Link>
-            {" for free."}
-          </p>
-        </Form>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
