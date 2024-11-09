ab7404109e00b1e40db84371b3a90592c53c179e
onissen <56229919+onissen@users.noreply.github.com>
2024-11-09 22:43:49 +0100

    chore(database): Rethink Package structure
    drizzle.config on package root level is needed to let drizzle CLI work properly, all schema definitions have to be defined in single files in schema/ directory

--- packages/database/drizzle.config.js
+++ packages/database/drizzle.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env.local" });
 
 export default defineConfig({
-  schema: "./drizzle/schema.js",
+  schema: "./schema",
   out: "./drizzle-migrations",
   dialect: "postgresql",
   dbCredentials: {
--- packages/database/drizzle/schema.js
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
-
-export const table = pgTable("User", {
-  id: serial("id").primaryKey(),
-  email: varchar("email", { length: 64 }),
-  password: varchar("password", { length: 64 }),
-  name: varchar("name", { length: 150 }),
-});
