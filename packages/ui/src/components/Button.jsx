786c28b3ef2d28dd6acf76f714c5604958590484
onissen <56229919+onissen@users.noreply.github.com>
2024-11-12 11:27:46 +0100

    feat(ui): New Class Merge Function cn
    Im UI-Package gibt es jetzt die Funktion cn() die mit clsx und tailwind-merge den Umgang mit Default-Classes und Property Classes steuern soll

--- packages/ui/src/components/Button.jsx
+++ packages/ui/src/components/Button.jsx
@@ -1,3 +1,7 @@
-export const Button = () => {
-  return <button>Boop Button</button>;
+import { cn } from "../cn";
+
+export const Button = ({ className }) => {
+  return (
+    <button className={cn("ui-bg-yellow-500", className)}>Boop Button</button>
+  );
 };
--- packages/ui/src/components/HelloWorld.jsx
+++ packages/ui/src/components/HelloWorld.jsx
@@ -1,7 +1,11 @@
-export function HelloWorld() {
+import { cn } from "../cn";
+import { Button } from "./Button";
+
+export function HelloWorld({ className }) {
   return (
-    <h1 className="ui-text-4xl ui-font-medium ui-bg-finance-300 ui-size-10">
-      Hello World
-    </h1>
+    <div className={cn(className, "ui-text-cockpit-300")}>
+      <h1 className="ui-text-4xl ui-font-medium">Hello World</h1>
+      <Button className="ui-text-4xl ui-bg-slate-500" />
+    </div>
   );
 }
