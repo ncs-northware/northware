ffcaf2f77ca93455527369b0b531aaaeb1cd6931
onissen <56229919+onissen@users.noreply.github.com>
2024-11-12 14:09:19 +0100

    chore(ui): New location for cn definition file to get a better organized package

--- packages/ui/src/components/HelloWorld.jsx
+++ packages/ui/src/components/HelloWorld.jsx
@@ -1,11 +1,9 @@
-import { cn } from "../cn";
-import { Button } from "./Button";
+import { cn } from "../lib/cn";
 
 export function HelloWorld({ className }) {
   return (
     <div className={cn(className, "ui-text-cockpit-300")}>
       <h1 className="ui-text-4xl ui-font-medium">Hello World</h1>
-      <Button className="ui-text-4xl ui-bg-slate-500" />
     </div>
   );
 }
