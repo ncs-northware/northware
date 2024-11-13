b71ef3a753bebc3cb52c2859cf99288d19dfe174
onissen <56229919+onissen@users.noreply.github.com>
2024-11-13 19:50:24 +0100

    feat(ui): Use TypeScript in @northware/ui
    @northware/ui nutzt jetzt an einigen Stellen TypeScript. Die tsconfig.json greift dazu auf das react-library Preset aus @northware/tsconfig zu.
    Die wichtigen Funktionen und Componenten nutzen bereits TypeScript. Nur die Tailwind COnfig muss noch umgestellt werden.

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -2,60 +2,78 @@ import * as React from "react";
 
 import { cn } from "../utils/cn";
 
-const Card = ({ className, ...props }, ref) => (
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
       "rounded-md border bg-card text-card-foreground shadow-sm",
-      className,
+      className
     )}
     {...props}
   />
-);
+));
 Card.displayName = "Card";
 
-const CardHeader = ({ className, ...props }, ref) => (
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("flex flex-col space-y-1.5 p-6", className)}
     {...props}
   />
-);
+));
 CardHeader.displayName = "CardHeader";
 
-const CardTitle = ({ className, ...props }, ref) => (
+const CardTitle = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
       "text-2xl font-semibold leading-none tracking-tight",
-      className,
+      className
     )}
     {...props}
   />
-);
+));
 CardTitle.displayName = "CardTitle";
 
-const CardDescription = ({ className, ...props }, ref) => (
+const CardDescription = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-);
+));
 CardDescription.displayName = "CardDescription";
 
-const CardContent = ({ className, ...props }, ref) => (
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-);
+));
 CardContent.displayName = "CardContent";
 
-const CardFooter = ({ className, ...props }, ref) => (
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("flex items-center p-6 pt-0", className)}
     {...props}
   />
-);
+));
 CardFooter.displayName = "CardFooter";
 
 export {
