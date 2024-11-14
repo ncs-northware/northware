602f11e3623aac573a93f87ff655a55b1d5c3159
onissen <56229919+onissen@users.noreply.github.com>
2024-11-14 12:47:25 +0100

    feat(root): Add TypeScript Support to root files / Rewrite commitlint.config.ts

---
+++ commitlint.config.ts
@@ -0,0 +1,22 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      RuleConfigSeverity.Warning,
+      "always",
+      ["chore", "docs", "feat", "fix", "revert"],
+    ],
+    "scope-max-length": [RuleConfigSeverity.Warning, "always", 40],
+  },
+  ignores: [
+    (commitMessage) => {
+      // add an exception for github
+      return /^Merge branch '.*' into [a-zA-Z0-9\/\-_]+$/.test(commitMessage);
+    },
+  ],
+};
+
+export default Configuration;
