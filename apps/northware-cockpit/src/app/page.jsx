c4e888e01e9d31df374e3f70f36ab437342e6a1c
onissen <56229919+onissen@users.noreply.github.com>
2024-11-14 17:56:42 +0100

    chore(cockpit): Prettier Formatting Run with new Plugin

--- apps/northware-cockpit/src/app/page.jsx
+++ apps/northware-cockpit/src/app/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1 className="bg-cockpit-500 text-3xl size-16">Northware Cockpit</h1>
+      <h1 className="size-16 bg-cockpit-500 text-3xl">Northware Cockpit</h1>
       <Link href="/dashboard">Dashboard</Link>
     </div>
   );
