23ac711a8dffc7bdf7628ad7bd2975c3cb348de8
onissen <56229919+onissen@users.noreply.github.com>
2024-11-19 17:19:57 +0100

    chore(cockpit): No specific Tailwind Configs for northware-cockpit, only using shareable config

--- apps/northware-cockpit/tailwind.config.ts
+++ apps/northware-cockpit/tailwind.config.ts
@@ -1,17 +1,9 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@northware/tailwind-config";
-import colors from "tailwindcss/colors";
 
 const config: Pick<Config, "content" | "presets" | "theme"> = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   presets: [sharedConfig],
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.sky,
-      },
-    },
-  },
 };
 
 export default config;
