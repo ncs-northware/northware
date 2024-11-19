761f53f16b83ff916200da5aa7f0ab21e76efc92
onissen <56229919+onissen@users.noreply.github.com>
2024-11-19 17:18:17 +0100

    feat(tailwind-config): Use external config file to define colors

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
+import colorConfig from "./colors";
 import animate from "tailwindcss-animate";
 
 const config: Omit<Config, "content"> = {
+  darkMode: ["class"],
+  presets: [colorConfig],
   theme: {
     container: {
       center: true,
@@ -14,25 +16,6 @@ const config: Omit<Config, "content"> = {
     extend: {
       fontFamily: {
         sans: ["var(--font-sourcesans)"],
-      },
-      colors: {
-        cockpit: colors.sky,
-        finance: colors.red,
-        trader: colors.orange,
-        secondary: {
-          DEFAULT: "#1E1E1E",
-          50: "#f6f6f6",
-          100: "#e7e7e7",
-          200: "#d1d1d1",
-          300: "#b0b0b0",
-          400: "#888888",
-          500: "#6d6d6d",
-          600: "#5d5d5d",
-          700: "#4f4f4f",
-          800: "#454545",
-          900: "#3d3d3d",
-          950: "#1e1e1e",
-        },
       },
       keyframes: {
         "accordion-down": {
