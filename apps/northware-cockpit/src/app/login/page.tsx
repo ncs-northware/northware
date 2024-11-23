74051d8cc90dea7e849d35d1567517a7d8092a7c
onissen <56229919+onissen@users.noreply.github.com>
2024-11-23 15:50:14 +0100

    feat(ui/cockpit): New LoginForm
    Das Login Form und die Login Page im Cockpit sind jetzt neu gestyled im Light und Dark Mode

--- apps/northware-cockpit/src/app/login/page.tsx
+++ apps/northware-cockpit/src/app/login/page.tsx
@@ -1,6 +1,5 @@
-import { LoginForm } from "@northware/ui/components";
+import { LoginForm, LoginWrapper } from "@northware/ui/components";
 import { signIn } from "@northware/auth/auth";
-import Image from "next/image";
 
 export default function LoginPage() {
   async function handleSubmit(values: any) {
@@ -13,20 +12,8 @@ export default function LoginPage() {
   }
 
   return (
-    <main className={`${""} ${""}`}>
-      <section className={""}>
-        <div id="loginForm">
-          <Image
-            src="/img/logo.svg"
-            height={150}
-            width={450}
-            className="mx-auto mb-6"
-            alt="Northware Cockpit Logo"
-          />
-          <h1 className="mb-4 text-center">Login</h1>
-          <LoginForm onSubmit={handleSubmit} />
-        </div>
-      </section>
-    </main>
+    <LoginWrapper>
+      <LoginForm onSubmit={handleSubmit} />
+    </LoginWrapper>
   );
 }
--- packages/ui/src/components/LoginForm.tsx
+++ packages/ui/src/components/LoginForm.tsx
@@ -13,7 +13,9 @@ import {
 import { Input } from "./Input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signIn } from "@northware/auth/auth";
+import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "./card";
+import { useTheme } from "next-themes";
 
 const formSchema = z.object({
   email: z.string(),
@@ -21,44 +23,85 @@ const formSchema = z.object({
 });
 
 export function LoginForm({ onSubmit }: { onSubmit: (values: any) => void }) {
+  const theme = useTheme();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: { email: "", password: "" },
   });
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>E-Mail</FormLabel>
-              <FormControl>
-                <Input placeholder="kunde@northware.de" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="kunde@northware.de" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button size="lg" variant="default" type="submit">
-          Submit
-        </Button>
-      </form>
-    </Form>
+    <>
+      <Image
+        src={
+          theme.resolvedTheme === "dark"
+            ? "/img/logo-dark.svg"
+            : "/img/logo-light.svg"
+        }
+        height={150}
+        width={380}
+        className="mb-6"
+        alt="Northware Cockpit Logo"
+      />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid gap-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>E-Mail</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="kunde@northware.de"
+                          autoComplete="email"
+                          required
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  size="lg"
+                  variant="default"
+                  type="submit"
+                  className="w-full"
+                >
+                  Submit
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
+
+export function LoginWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center px-4">
+      {children}
+    </main>
   );
 }
