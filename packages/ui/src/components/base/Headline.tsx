8565a711b2f8187f4aebc85dcd60bb805b9dab9a
onissen <56229919+onissen@users.noreply.github.com>
2024-12-20 19:49:58 +0100

    fix(ui): Headline: Unused title Level
    Das Level title îst innerhalb der Headline Klasse nicht gestylet, Der level Prop sollte dies auch nicht anbieten.

--- packages/ui/src/components/base/Headline.tsx
+++ packages/ui/src/components/base/Headline.tsx
@@ -3,7 +3,7 @@ import { cn } from "@northware/ui/utils";
 
 interface HeadlineProps extends HTMLAttributes<HTMLHeadingElement> {
   children: ReactNode;
-  level: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "title";
+  level: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
 const basicClasses = "scroll-m-20 tracking-tight";
