12f2bdc3af695bbd39507b7934b62a7c7f1d61ad
onissen <56229919+onissen@users.noreply.github.com>
2024-12-21 19:17:20 +0100

    chore(cockpit): Just some page content adjustments

--- apps/northware-cockpit/src/app/page.tsx
+++ apps/northware-cockpit/src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <Container>
+    <>
       <Headline level="h1">Northware Cockpit Startseite</Headline>
       <Headline level="h3">
-        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/dashboard">Zum Dashboard</Link>
       </Headline>
-    </Container>
+    </>
   );
 }
