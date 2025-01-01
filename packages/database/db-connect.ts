585fc8a73b23762f676597cf9835c8b076f974ce
onissen <56229919+onissen@users.noreply.github.com>
2025-01-01 13:54:11 +0100

    fix(database): Fix Biome Error: noNonNullAssertion

--- packages/database/db-connect.ts
+++ packages/database/db-connect.ts
@@ -2,5 +2,5 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
 // Connection for Apps
-export const client = neon(`${process.env.DATABASE_URL!}?sslmode=require`);
+export const client = neon(`${process.env.DATABASE_URL || ''}?sslmode=require`);
 export const db = drizzle({ client });
