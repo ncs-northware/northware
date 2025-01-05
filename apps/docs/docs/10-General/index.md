151103a5921f4efc25758e88700abced234f1138
onissen <56229919+onissen@users.noreply.github.com>
2025-01-05 01:47:54 +0100

    feat(docs): Init Mintlify Docs

--- apps/docs/docs/10-General/index.md
+++
@@ -1,5 +0,0 @@
----
-pagination_prev: null
----
-
-# General
--- apps/docs/src/theme/MDXComponents.js
+++
@@ -1,8 +0,0 @@
-// Import the original mapper
-import MDXComponents from '@theme-original/MDXComponents';
-import { GithubBadge } from '../components/github-flavours';
-
-export default {
-  ...MDXComponents,
-  GithubBadge,
-};
