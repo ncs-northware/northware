423e158ca9a67e45cfeea96a7c2893672242769d
onissen <56229919+onissen@users.noreply.github.com>
2025-03-01 19:39:27 +0100

    feat(ui): Permission based Redering of Menu Items
    menuData ruft nur die Daten ab, für die der User die nötigen userPermissions hat (permissionKey), für User mit der Permission allAccess werden alle Menüpunkte angezeigt. Die Komponenten MainNav und MobileNav geben die Clerk User ID an menuData weiter. Damit werden dann dort mit getUserPermissions die Rechte des Nutzers abgefragt. Die MetaNav und MobileMetaNav erhalten das user Objekt jetzt als Parameter von MainNav und MobileNav. So wird currentUser nicht doppelt abgefragt.

--- packages/ui/src/components/menu/menu-data.ts
+++ packages/ui/src/components/menu/menu-data.ts
@@ -1,18 +1,28 @@
+import { getUserPermissions } from '@northware/auth/account';
 import { db } from '@northware/database/connection';
 import { mainNavTable } from '@northware/database/schema';
 import type { ServiceType } from '@northware/service-config';
-import { eq } from 'drizzle-orm';
+import { and, eq, inArray } from 'drizzle-orm';
 
-export async function menuData(service: ServiceType) {
+export async function menuData(service: ServiceType, userId?: string) {
+  const userPermissions = await getUserPermissions(userId);
   const result = await db
     .select({
       itemId: mainNavTable.itemId,
       title: mainNavTable.title,
       href: mainNavTable.href,
       childOf: mainNavTable.childOf,
+      permissionKey: mainNavTable.permissionKey,
     })
     .from(mainNavTable)
-    .where(eq(mainNavTable.app, service))
+    .where(
+      and(
+        eq(mainNavTable.app, service),
+        userPermissions.includes('allAccess')
+          ? undefined
+          : inArray(mainNavTable.permissionKey, userPermissions)
+      )
+    )
     .orderBy(mainNavTable.order);
 
   const topLevelItems = result.filter((item) => item.childOf == null);
