273c5e086f9c006d283c8377a4d6c229447d4950
onissen <56229919+onissen@users.noreply.github.com>
2025-03-23 17:31:36 +0100

    feat(cockpit): Use new Tailwind and shadcn Monorepo
    - Dem Beispiel von shadcn folgend hat die App keine eigene CSS-Datei mehr
    - Tailwind und alle Plugins werden über @northware/ui bereitgestellt
    - Das Root-Layout nutzt dem Beispiel von shadcn folgend React 19

--- apps/northware-cockpit/src/app/layout.tsx
+++ apps/northware-cockpit/src/app/layout.tsx
@@ -1,6 +1,5 @@
-import './globals.css';
 import '@northware/ui/css';
-import { UIProvider } from '@northware/ui/components/providers/ui-provider';
+import { GeneralProvider } from '@northware/ui/components/providers/general-provider';
 import { source_sans } from '@northware/ui/lib/fonts';
 export const metadata = {
   title: {
@@ -11,13 +10,13 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html className="theme-cockpit" lang="de" suppressHydrationWarning>
       <body className={`${source_sans.variable} font-sans`}>
-        <UIProvider>{children}</UIProvider>
+        <GeneralProvider>{children}</GeneralProvider>
       </body>
     </html>
   );
