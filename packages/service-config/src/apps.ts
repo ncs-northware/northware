01754b6435d88eaf3e3d31469d063b1f832b6b1a
onissen <56229919+onissen@users.noreply.github.com>
2025-03-31 19:48:15 +0200

    feat(service-config): Control storybookThemes Array from @northware/service-config

--- packages/service-config/src/apps.ts
+++ packages/service-config/src/apps.ts
@@ -28,3 +28,41 @@ export const suiteAppsMeta: {
     slug: "admin",
   },
 ];
+
+export const storybookThemes = {
+  default: "theme-cockpit",
+  items: [
+    {
+      title: "Cockpit Light",
+      value: "theme-cockpit",
+    },
+    {
+      title: "Cockpit Dark",
+      value: "theme-cockpit dark",
+    },
+    {
+      title: "Finance Light",
+      value: "theme-finance",
+    },
+    {
+      title: "Finance Dark",
+      value: "theme-finance dark",
+    },
+    {
+      title: "Trader Light",
+      value: "theme-trader",
+    },
+    {
+      title: "Trader Dark",
+      value: "theme-trader dark",
+    },
+    {
+      title: "Admin Panel Light",
+      value: "theme-admin",
+    },
+    {
+      title: "Admin Panel Dark",
+      value: "theme-admin dark",
+    },
+  ],
+};
