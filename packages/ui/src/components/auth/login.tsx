3caea56e71ed9e8fd4473f228ee2f14929117654
onissen <56229919+onissen@users.noreply.github.com>
2025-04-08 17:26:09 +0200

    fix(ui): LoginForm changes: Remove LoginWrapper Component, Add ThemeSwitch, Add Edit Idetifier logic
    Die LoginWrapper Komponente war ein gestyletes main Element das children akzeptierte. Dieses Element wurde jetzt in die Komponente LoginForm integriert. So ist LoginWrapper nicht mehr nötig. Es wurde jetzt ein ThemeSwitch in das LoginForm integriert, sodass sich das Theme auch auf der Login-Page wechseln lässt. Bei der Passwort-Eingabe hat der User die Möglichkeit zur Eingabe der Mail-Adresse zurückzukehren.

--- packages/ui/src/components/auth/login.tsx
+++ packages/ui/src/components/auth/login.tsx
@@ -7,6 +7,8 @@ import { Brand } from "@northware/ui/components/base/brand";
 import { Button } from "@northware/ui/components/base/button";
 import { Input } from "@northware/ui/components/form-parts/input";
 import { Label } from "@northware/ui/components/form-parts/label";
+import { navigationMenuButtonStyle } from "@northware/ui/components/menu/navigation-menu";
+import { ThemeSwitch } from "@northware/ui/components/menu/theme-switch";
 import { Alert, AlertDescription } from "@northware/ui/components/panels/alert";
 import {
   Card,
@@ -15,76 +17,28 @@ import {
   CardHeader,
   CardTitle,
 } from "@northware/ui/components/panels/card";
-import { LoaderCircle } from "lucide-react";
+import { LoaderCircle, PencilIcon } from "lucide-react";
 
 export function LoginForm({ service }: { service: ServiceType }) {
   return (
-    <SignIn.Root>
-      <Brand className="mb-6 text-2xl" iconWidth="w-14" service={service} />
-      <Clerk.Loading>
-        {(isGlobalLoading) => (
-          <>
-            <SignIn.Step name="start" className="w-full max-w-sm">
-              <Card>
-                <CardHeader>
-                  <CardTitle className="text-2xl">Login</CardTitle>
-                </CardHeader>
-                <CardContent className="">
-                  <Clerk.Field name="identifier" className="grid gap-4">
-                    <Clerk.Label asChild>
-                      <Label>Email</Label>
-                    </Clerk.Label>
-                    <Clerk.Input type="email" required asChild>
-                      <Input placeholder="kunde@northware.de" />
-                    </Clerk.Input>
-                    <Clerk.FieldError>
-                      {({ message, code }) => (
-                        <LoginErrorAlert code={code} message={message} />
-                      )}
-                    </Clerk.FieldError>
-                  </Clerk.Field>
-                </CardContent>
-                <CardFooter>
-                  <SignIn.Action submit asChild>
-                    <Button
-                      disabled={isGlobalLoading}
-                      size="lg"
-                      variant="default"
-                      className="w-full"
-                    >
-                      <Clerk.Loading>
-                        {(isLoading) => {
-                          return isLoading ? (
-                            <LoaderCircle className="size-4 animate-spin" />
-                          ) : (
-                            "Weiter"
-                          );
-                        }}
-                      </Clerk.Loading>
-                    </Button>
-                  </SignIn.Action>
-                </CardFooter>
-              </Card>
-            </SignIn.Step>
-
-            <SignIn.Step name="verifications" className="w-full max-w-sm">
-              <SignIn.Strategy name="password">
+    <main className="flex h-screen w-full flex-col items-center justify-center px-4">
+      <SignIn.Root>
+        <Brand className="mb-6 text-2xl" iconWidth="w-14" service={service} />
+        <Clerk.Loading>
+          {(isGlobalLoading) => (
+            <>
+              <SignIn.Step name="start" className="w-full max-w-sm">
                 <Card>
                   <CardHeader>
-                    <CardTitle className="text-2xl">
-                      Herzlich Willkommen
-                    </CardTitle>
-                    <p className="text-muted-foreground text-sm leading-none">
-                      <SignIn.SafeIdentifier />
-                    </p>
+                    <CardTitle className="text-2xl">Login</CardTitle>
                   </CardHeader>
                   <CardContent>
-                    <Clerk.Field name="password" className="grid gap-4">
+                    <Clerk.Field name="identifier" className="grid gap-4">
                       <Clerk.Label asChild>
-                        <Label>Passwort</Label>
+                        <Label>Email</Label>
                       </Clerk.Label>
-                      <Clerk.Input type="password" asChild>
-                        <Input />
+                      <Clerk.Input type="email" required asChild>
+                        <Input placeholder="kunde@northware.de" />
                       </Clerk.Input>
                       <Clerk.FieldError>
                         {({ message, code }) => (
@@ -106,7 +60,7 @@ export function LoginForm({ service }: { service: ServiceType }) {
                             return isLoading ? (
                               <LoaderCircle className="size-4 animate-spin" />
                             ) : (
-                              "Anmelden"
+                              "Weiter"
                             );
                           }}
                         </Clerk.Loading>
@@ -114,12 +68,69 @@ export function LoginForm({ service }: { service: ServiceType }) {
                     </SignIn.Action>
                   </CardFooter>
                 </Card>
-              </SignIn.Strategy>
-            </SignIn.Step>
-          </>
-        )}
-      </Clerk.Loading>
-    </SignIn.Root>
+              </SignIn.Step>
+
+              <SignIn.Step name="verifications" className="w-full max-w-sm">
+                <SignIn.Strategy name="password">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle className="text-2xl">
+                        Herzlich Willkommen
+                      </CardTitle>
+                      <p className="flex items-center text-muted-foreground text-sm leading-none">
+                        <SignIn.SafeIdentifier />
+                        <SignIn.Action asChild navigate="start">
+                          <PencilIcon className="ml-3 size-4 cursor-pointer" />
+                        </SignIn.Action>
+                      </p>
+                    </CardHeader>
+                    <CardContent>
+                      <Clerk.Field name="password" className="grid gap-4">
+                        <Clerk.Label asChild>
+                          <Label>Passwort</Label>
+                        </Clerk.Label>
+                        <Clerk.Input type="password" asChild>
+                          <Input />
+                        </Clerk.Input>
+                        <Clerk.FieldError>
+                          {({ message, code }) => (
+                            <LoginErrorAlert code={code} message={message} />
+                          )}
+                        </Clerk.FieldError>
+                      </Clerk.Field>
+                    </CardContent>
+                    <CardFooter>
+                      <SignIn.Action submit asChild>
+                        <Button
+                          disabled={isGlobalLoading}
+                          size="lg"
+                          variant="default"
+                          className="w-full"
+                        >
+                          <Clerk.Loading>
+                            {(isLoading) => {
+                              return isLoading ? (
+                                <LoaderCircle className="size-4 animate-spin" />
+                              ) : (
+                                "Anmelden"
+                              );
+                            }}
+                          </Clerk.Loading>
+                        </Button>
+                      </SignIn.Action>
+                    </CardFooter>
+                  </Card>
+                </SignIn.Strategy>
+              </SignIn.Step>
+            </>
+          )}
+        </Clerk.Loading>
+      </SignIn.Root>
+      <ThemeSwitch
+        className={`mt-3 text-muted-foreground ${navigationMenuButtonStyle()}`}
+        withDescriptionText
+      />
+    </main>
   );
 }
 
@@ -143,13 +154,5 @@ function LoginErrorAlert({ code, message }: { code: string; message: string }) {
         <p>{alertMessage()}</p>
       </AlertDescription>
     </Alert>
-  );
-}
-
-export function LoginWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="flex h-screen w-full flex-col items-center justify-center px-4">
-      {children}
-    </main>
   );
 }
