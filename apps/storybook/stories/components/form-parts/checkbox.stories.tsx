06a7200cd651710df1531fa398386ccea1a73498
onissen <56229919+onissen@users.noreply.github.com>
2025-04-09 13:38:09 +0200

    docs(storybook): Docs for Form Parts Components
    Es gibt jetzt Docs für die Form Parts Komponenten: Checkbox, Form, Input, Label, Select, Switch

---
+++ apps/storybook/stories/components/form-parts/checkbox.stories.tsx
@@ -0,0 +1,42 @@
+import { Checkbox } from "@northware/ui/components/form-parts/checkbox";
+import { Label } from "@northware/ui/components/form-parts/label";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Checkbox> = {
+  title: "Components/Form Parts/Checkbox",
+  component: Checkbox,
+  argTypes: {},
+  args: {
+    id: "terms",
+    disabled: false,
+  },
+  render: (args) => (
+    <div className="flex space-x-2">
+      <Checkbox {...args} />
+      <Label htmlFor="terms">Accept terms and conditions</Label>
+    </div>
+  ),
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/**
+ * Die Standard Checkbox mit einem Label
+ */
+export const Default: Story = {};
+
+/**
+ * Mit dem `disabled` Prop lässt sich die Checkbox deaktivieren.
+ */
+export const Disabled: Story = {
+  args: {
+    id: "disabled-terms",
+    disabled: true,
+  },
+};
---
+++ apps/storybook/stories/components/form-parts/input.stories.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@northware/ui/components/base/button";
+import { Input } from "@northware/ui/components/form-parts/input";
+import { Label } from "@northware/ui/components/form-parts/label";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Components/Form Parts/Input",
+  component: Input,
+  argTypes: {},
+  args: {
+    type: "email",
+    placeholder: "Email",
+    disabled: false,
+    className: "w-96",
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/**
+ * Die Standard-Form eines Inputs
+ */
+export const Default: Story = {};
+
+/**
+ * Ein Input-Feld mit dem Dateien von der Festplatte ausgewählt werden können
+ */
+export const File: Story = {
+  args: { type: "file" },
+};
+/**
+ * Das `disabled` Prop kann genutzt werden, um das Eingabefeld nicht interaktiv
+ * zu machen und es verblasst erscheinen zu lassen, was darauf hinweist, dass
+ * derzeit keine Eingabe akzeptiert wird.
+ */
+export const Disabled: Story = {
+  args: { disabled: true },
+};
+
+/**
+ * Die `Label` Komponente kann genutzt werden um ein klares beschreibendes Label über
+ * oder neben dem Input-Feld anzuzeigen, um den User zu unterstützen.
+ */
+export const WithLabel: Story = {
+  render: (args) => (
+    <div className="grid items-center gap-1.5">
+      <Label htmlFor="email">{args.placeholder}</Label>
+      <Input {...args} id="email" />
+    </div>
+  ),
+};
+
+/**
+ * Um weitere Anweisungen oder Informationen an den User zu geben,
+ * kann ein Text-Element unter dem Input-Feld genutzt werden.
+ */
+export const WithHelperText: Story = {
+  render: (args) => (
+    <div className="grid items-center gap-1.5">
+      <label htmlFor="email-2">{args.placeholder}</label>
+      <Input {...args} id="email-2" />
+      <p className="text-foreground/50 text-sm">Enter your email address.</p>
+    </div>
+  ),
+};
+
+/**
+ * Die `Button` Komponente kann genutzt werden, um zu zeigen, dass das Input-Feld
+ * als Teil einer Action verwendet werden kann.
+ */
+export const WithButton: Story = {
+  render: (args) => (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input {...args} />
+      <Button variant="default" type="submit">
+        Subscribe
+      </Button>
+    </div>
+  ),
+};
