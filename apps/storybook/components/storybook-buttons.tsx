ea8ed6e2a8f7784b33830c5ac1143ce22e7fbb7e
onissen <56229919+onissen@users.noreply.github.com>
2025-04-11 13:58:14 +0200

    fix(cockpit/storybook): Use @northware/ui without src/ directory

--- apps/storybook/components/storybook-buttons.tsx
+++ apps/storybook/components/storybook-buttons.tsx
@@ -3,7 +3,7 @@ import { GithubIcon, ShadcnuiIcon } from "@northware/ui/icons";
 import Link from "next/link";
 export const ViewSource = ({ path }: { path: string }) => {
   const baseurl =
-    "https://github.com/ncs-northware/northware/tree/main/packages/ui/src";
+    "https://github.com/ncs-northware/northware/tree/main/packages/ui";
   return (
     <Button asChild variant="secondary" size="sm">
       <Link href={`${baseurl}/${path}`} target="_blank">
