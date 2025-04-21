92cddf9b65db0ac1bc11fb94ca47873181aaa83d
onissen <56229919+onissen@users.noreply.github.com>
2025-04-21 18:20:22 +0200

    fix(service-config): Remove href Element from suiteAppsMeta
    Der href der Apps wird in den Navigationen jetzt über Environment Variablen definiert. Der Name dieser Variablen wir über app.slug zusammengesetzt. app.href wird nicht mehr benötigt.

--- packages/service-config/src/apps.ts
+++ packages/service-config/src/apps.ts
@@ -2,29 +2,24 @@ import type { ServiceType } from "./types";
 
 export const suiteAppsMeta: {
   title: string;
-  href: string;
   slug: ServiceType;
 }[] = [
   // Attribute der Navigationspunkte der AppSwitches in MetaNav
   //   TODO: permission-based rendering
   {
     title: "Northware Cockpit",
-    href: "http://localhost:40110",
     slug: "cockpit",
   },
   {
     title: "Northware Finance",
-    href: "http://localhost:40120",
     slug: "finance",
   },
   {
     title: "Northware Trader",
-    href: "http://localhost:40130",
     slug: "trader",
   },
   {
-    title: "Admin Panel",
-    href: "http://localhost:40100",
+    title: "Northware Admin",
     slug: "admin",
   },
 ];
