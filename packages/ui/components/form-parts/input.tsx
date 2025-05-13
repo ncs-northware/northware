8f8769881024487bd1d2e121383c88d2bb1833c4
onissen <56229919+onissen@users.noreply.github.com>
2025-05-13 23:20:31 +0200

    fix(ui): simplify Input Component
    Die inputVariants wurden von der Komponente nicht verwendet. Die Definition wurde daher entfernt.

--- packages/ui/components/form-parts/input.tsx
+++ packages/ui/components/form-parts/input.tsx
@@ -1,26 +1,6 @@
 import { cn } from "@northware/ui/lib/utils";
-import { type VariantProps, cva } from "class-variance-authority";
-import type * as React from "react";
 
-const inputVariants = cva(
-  "flex h-10 w-full rounded-md border border-input bg-background text-base ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-  {
-    variants: {
-      dimension: {
-        default: "px-4 py-3",
-        lg: "text-lg sm:p-5",
-        sm: "px-3 py-2 text-sm",
-      },
-    },
-    defaultVariants: { dimension: "default" },
-  }
-);
-
-function Input({
-  className,
-  type,
-  ...props
-}: React.ComponentProps<"input"> & VariantProps<typeof inputVariants>) {
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
     <input
       type={type}
