ebea730bc5a834231915c0c7dedc59823238a8f2
onissen <56229919+onissen@users.noreply.github.com>
2025-05-17 01:39:06 +0200

    fix(ui): biome hatte vorgeschlagen, die toast Funktion von sonner über export from bereitzustellen

--- packages/ui/components/panels/sonner.tsx
+++ packages/ui/components/panels/sonner.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { Toaster as Sonner, type ToasterProps, toast } from "sonner";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
@@ -23,4 +23,5 @@ const Toaster = ({ ...props }: ToasterProps) => {
   );
 };
 
-export { Toaster, toast };
+export { Toaster };
+export { toast } from "sonner";
