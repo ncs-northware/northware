af3cffcbdbf69b874fb097f3aa41409a15b7674c
onissen <56229919+onissen@users.noreply.github.com>
2025-05-22 23:29:44 +0200

    fix(ui): manuData only fetches mainInclude Menu Items

--- packages/ui/lib/menu-data.ts
+++ packages/ui/lib/menu-data.ts
@@ -18,6 +18,7 @@ export async function menuData(service: ServiceType, userId?: string) {
     .where(
       and(
         eq(mainNavTable.app, service),
+        eq(mainNavTable.mainInclude, true),
         userPermissions.includes("allAccess")
           ? undefined
           : inArray(mainNavTable.permissionKey, userPermissions)
