8b19e64712382571d7aaeb43d081070785fce2e7
onissen <56229919+onissen@users.noreply.github.com>
2025-05-23 13:25:20 +0200

    feat(cockpit): Use SidebarLayout everywhere in cockpit
    Alle Seiten im Cockpit nutzen jetzt das neue SidebarLayout

--- apps/northware-cockpit/src/app/admin/user/create/page.tsx
+++ apps/northware-cockpit/src/app/admin/user/create/page.tsx
@@ -1,13 +1,20 @@
 import CreateUserForm from "@/components/create-user-form";
 import { createUser } from "@/lib/user-actions";
-import { Container } from "@northware/ui/components/container";
 import { Headline } from "@northware/ui/components/headline";
+import { SidebarLayout } from "@northware/ui/components/sidebar-layout";
 
 export default function CreateUserPage() {
   return (
-    <Container service="admin">
+    <SidebarLayout
+      service="cockpit"
+      breadcrumbs={[
+        { label: "Admin Panel", href: "/admin" },
+        { label: "Benutzerverwaltung", href: "/admin/user" },
+        { label: "Benutzer erstellen", href: "create", active: true },
+      ]}
+    >
       <Headline level="h1">Benutzer erstellen</Headline>
       <CreateUserForm createUser={createUser} />
-    </Container>
+    </SidebarLayout>
   );
 }
