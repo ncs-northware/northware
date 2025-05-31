027c79ce9363838410e1ff45662d4cdd424a9902
onissen <56229919+onissen@users.noreply.github.com>
2025-05-31 15:53:32 +0200

    feat(ui): Enable Source Code Pro as Font Family for font-mono

--- packages/ui/lib/fonts.ts
+++ packages/ui/lib/fonts.ts
@@ -1,7 +1,13 @@
-import { Source_Sans_3 } from "next/font/google";
+import { Source_Code_Pro, Source_Sans_3 } from "next/font/google";
 
 export const source_sans = Source_Sans_3({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-sourcesans",
+});
+
+export const source_code = Source_Code_Pro({
+  subsets: ["latin"],
+  display: "optional",
+  variable: "--font-sourcecode",
 });
