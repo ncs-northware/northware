d63f877e5c3269a41016ad71de59fcc72a87bff2
onissen <56229919+onissen@users.noreply.github.com>
2025-06-01 16:10:19 +0200

    fix(ui): Alert Grid Layout
    In Commit 0c6404e7b807995cf9b8dfcae409aab098fe11ca war das Grid-Layout der Alert Komponente entfernt worden. Dieses wurde nun wieder hinzugefügt. Um einen Alert richtig darzustellen, sollten AlertDescription und ggf. AlertTitle genutzt werden.

--- packages/ui/components/alert.tsx
+++ packages/ui/components/alert.tsx
@@ -8,7 +8,7 @@ import {
 } from "lucide-react";
 
 const alertVariants = cva(
-  "relative w-full items-start gap-y-0.5 rounded-lg border px-4 py-3 text-sm has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] has-[>svg]:gap-x-3 [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",
+  "relative grid w-full grid-cols-[0_1fr] items-start gap-y-0.5 rounded-lg border px-4 py-3 text-sm has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] has-[>svg]:gap-x-3 [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",
   {
     variants: {
       variant: {
@@ -64,7 +64,7 @@ function AlertDescription({
     <div
       data-slot="alert-description"
       className={cn(
-        "col-start-2 grid justify-items-start gap-1 text-muted-foreground text-sm [&_p]:leading-relaxed",
+        "col-start-2 grid justify-items-start gap-1 text-sm [&_p]:leading-relaxed",
         className
       )}
       {...props}
