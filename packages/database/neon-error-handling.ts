091d5aeda62cce69503164c5ff4c197f1195127f
onissen <56229919+onissen@users.noreply.github.com>
2025-06-09 00:33:22 +0200

    fix(database): Custom Error Handling for error Code 23505 (unique_violation)
    Die Funktion gibt jetzt eine Custom Error Message aus, wenn ein Eintrag bereits vorhanden ist, und so ein unique constraint verletzt wird (Postgres Error Code 23505, unique violation)

--- packages/database/neon-error-handling.ts
+++ packages/database/neon-error-handling.ts
@@ -7,6 +7,11 @@ export function handleNeonError(error: unknown) {
         throw new Error(
           `Eine Fremdschlüssel-Bedingung in der Tabelle ${error.table} wird nicht erfüllt.`
         );
+      case "23505":
+        throw new Error(
+          `Ein Eintrag mit diesen Daten ist bereits vorhanden. (${error.detail})`
+        );
+
       default:
         throw new Error(
           `Es ist ein Fehler in der Datenbank aufgetreten: ${error.message} Der Fehlercode lautet ${error.code}`
