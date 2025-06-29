4c55bb29a0eb8cbdb38c27bdd279f0f7bbdf81b0
onissen <56229919+onissen@users.noreply.github.com>
2025-06-29 16:41:11 +0200

    feat(cockpit): Use PermissionProvider on /admin/user
    Der Inhalt der Seite /admin/user wird nur angezeigt, wenn der Nutzer die Berechtigung cockpit::user.read hat. Andernfalls zeigt die Komponente eine Fehler-Seite.

--- apps/cockpit/src/app/admin/user/page.tsx
+++ apps/cockpit/src/app/admin/user/page.tsx
@@ -7,6 +7,7 @@ import {
 import { Button } from "@northware/ui/components/button";
 import { DataTable } from "@northware/ui/components/data-table";
 import { Headline } from "@northware/ui/components/headline";
+import { PermissionProvider } from "@northware/ui/components/permission-provider";
 import { SidebarLayout } from "@northware/ui/components/sidebar-layout";
 import { LightbulbIcon } from "@northware/ui/icons/lucide";
 import Link from "next/link";
@@ -25,34 +26,36 @@ export default async function Page() {
         { label: "Benutzerverwaltung", href: "/admin/user", active: true },
       ]}
     >
-      <div className="flex justify-between gap-4">
-        <div>
-          <Headline level="h1">Benutzerverwaltung</Headline>
-          <p className="mb-4 text-justify font-medium text-muted-foreground">
-            Ein Benutzerkonto ist eine digitale Identität, mit der sich eine
-            Person in den Northware Apps authentifizieren kann.
-          </p>
-        </div>
+      <PermissionProvider permissionKey="cockpit::user.read">
+        <div className="flex justify-between gap-4">
+          <div>
+            <Headline level="h1">Benutzerverwaltung</Headline>
+            <p className="mb-4 text-justify font-medium text-muted-foreground">
+              Ein Benutzerkonto ist eine digitale Identität, mit der sich eine
+              Person in den Northware Apps authentifizieren kann.
+            </p>
+          </div>
 
-        {/* TODO Nur mit Berechtigung create User */}
-        <Button variant="default">
-          <Link href="user/create">Benutzer hinzufügen</Link>
-        </Button>
-      </div>
-      <DataTable
-        columns={columns}
-        data={userArray}
-        withRowSelect={false}
-        initialSorting="fullName"
-      />
-      <Alert>
-        <LightbulbIcon className="size-4" />
-        <AlertTitle>Tipp</AlertTitle>
-        <AlertDescription>
-          Die eigenen Benutzerdaten können über das Benutzerprofil geändert
-          werden.
-        </AlertDescription>
-      </Alert>
+          {/* TODO Nur mit Berechtigung create User */}
+          <Button variant="default">
+            <Link href="user/create">Benutzer hinzufügen</Link>
+          </Button>
+        </div>
+        <DataTable
+          columns={columns}
+          data={userArray}
+          withRowSelect={false}
+          initialSorting="fullName"
+        />
+        <Alert>
+          <LightbulbIcon className="size-4" />
+          <AlertTitle>Tipp</AlertTitle>
+          <AlertDescription>
+            Die eigenen Benutzerdaten können über das Benutzerprofil geändert
+            werden.
+          </AlertDescription>
+        </Alert>
+      </PermissionProvider>
     </SidebarLayout>
   );
 }
