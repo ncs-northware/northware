fd21cd5dd0c1785dbc65fe2e3b00a28425270519
onissen <56229919+onissen@users.noreply.github.com>
2025-06-29 16:32:02 +0200

    feat(auth): New Function userHasPermission
    Mit der Funktion userHasPermission kann geprüft werden, ob der aktuelle Benutzer die benötigte Berechtigung (permissionKey) hat. Die Funktion kann in den Apps verwendet werden, um die UI mit Bedingungen anzuzeigen. Um ganze Seiten basierend auf dieser Funktion anzuzeigen, kann die UI Komponente PermissionProvider verwendet werden. Hat der Nutzer die benötigte Berechtigung nicht, wird eine Fehler-Seite angezeigt.

--- packages/auth/src/account.ts
+++ packages/auth/src/account.ts
@@ -5,8 +5,10 @@ import {
   rolesToAccounts,
 } from "@northware/database/schema";
 import { eq } from "drizzle-orm";
+import { cache } from "react";
+import { currentUser } from "./server";
 
-export async function getUserPermissions(clerkUserId?: string) {
+export const getUserPermissions = cache(async (clerkUserId?: string) => {
   /* Diese Funktion gibt alle permissionKeys eines Benutzers aus Rollen (rolesToAccounts und PermissionsToRoles) und aus 
   zusätzlichen Berechtigungen (PermissionsToAccounts) zurück. */
   const user = clerkUserId || "";
@@ -39,6 +41,12 @@ export async function getUserPermissions(clerkUserId?: string) {
   ];
 
   return userPermissions;
+});
+
+export async function userHasPermission(permissionKey: string) {
+  const user = await currentUser();
+  const permissions = await getUserPermissions(user?.id);
+  return permissions.includes(permissionKey);
 }
 
 export async function getUserRoles(
