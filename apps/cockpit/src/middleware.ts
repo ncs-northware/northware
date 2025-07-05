6975fa5a72b7eed90ec81f0ff42e6f9ecf131db6
onissen <56229919+onissen@users.noreply.github.com>
2025-07-05 16:45:22 +0200

    chore: remove fixed todos

--- apps/cockpit/src/middleware.ts
+++ apps/cockpit/src/middleware.ts
@@ -12,8 +12,6 @@ export default clerkMiddleware(async (auth, request) => {
   }
 });
 
-// TODO: Prüfung, ob der Nutzer die nötigen Berechtigungen hat um die App zu nutzen, ansonsten auf eine Seite weiterleiten auf der der Nutzer eine andere App wählen kann
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
