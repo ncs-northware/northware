0ec9145ec43da848cee4eb55ebc3c9aa8bde41d3
onissen <56229919+onissen@users.noreply.github.com>
2025-07-09 21:55:10 +0200

    chore(ui): formatting

--- packages/ui/components/theme-switch.tsx
+++ packages/ui/components/theme-switch.tsx
@@ -6,50 +6,50 @@ import { useTheme } from "next-themes";
 import { useCallback } from "react";
 
 export function ThemeSwitch({
-	as,
-	className,
+  as,
+  className,
 }: {
-	className?: string;
-	as: "icon" | "button";
+  className?: string;
+  as: "icon" | "button";
 }) {
-	const { setTheme, resolvedTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
 
-	const toggleTheme = useCallback(() => {
-		setTheme(resolvedTheme === "dark" ? "light" : "dark");
-	}, [resolvedTheme, setTheme]);
+  const toggleTheme = useCallback(() => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  }, [resolvedTheme, setTheme]);
 
-	if (as === "button") {
-		return (
-			<Button
-				className={className}
-				onClick={toggleTheme}
-				size="sm"
-				variant="secondary"
-			>
-				<SunIcon className="hidden group-[.storybook-darkTheme]:block [html.dark_&]:block" />
-				<MoonIcon className="hidden group-[.storybook-lightTheme]:block [html.light_&]:block" />
-				<span className="sr-only">Toggle theme</span>
-				<span className="ml-2" suppressHydrationWarning>
-					{resolvedTheme === "dark"
-						? "Helles Design aktivieren"
-						: "Dunkles Design aktivieren"}
-				</span>
-			</Button>
-		);
-	}
+  if (as === "button") {
+    return (
+      <Button
+        className={className}
+        onClick={toggleTheme}
+        size="sm"
+        variant="secondary"
+      >
+        <SunIcon className="hidden group-[.storybook-darkTheme]:block [html.dark_&]:block" />
+        <MoonIcon className="hidden group-[.storybook-lightTheme]:block [html.light_&]:block" />
+        <span className="sr-only">Toggle theme</span>
+        <span className="ml-2" suppressHydrationWarning>
+          {resolvedTheme === "dark"
+            ? "Helles Design aktivieren"
+            : "Dunkles Design aktivieren"}
+        </span>
+      </Button>
+    );
+  }
 
-	// as icon or other variants
+  // as icon or other variants
 
-	return (
-		<Button
-			className={className}
-			onClick={toggleTheme}
-			size="icon"
-			variant="ghost"
-		>
-			<SunIcon className="hidden group-[.storybook-darkTheme]:block [html.dark_&]:block" />
-			<MoonIcon className="hidden group-[.storybook-lightTheme]:block [html.light_&]:block" />
-			<span className="sr-only">Toggle theme</span>
-		</Button>
-	);
+  return (
+    <Button
+      className={className}
+      onClick={toggleTheme}
+      size="icon"
+      variant="ghost"
+    >
+      <SunIcon className="hidden group-[.storybook-darkTheme]:block [html.dark_&]:block" />
+      <MoonIcon className="hidden group-[.storybook-lightTheme]:block [html.light_&]:block" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
 }
