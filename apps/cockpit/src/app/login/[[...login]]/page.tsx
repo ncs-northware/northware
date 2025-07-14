b593931d229f071e5a2afa27f2494f28fd74856b
Oke <56229919+onissen@users.noreply.github.com>
2025-07-14 23:21:07 +0200

    Revert "Trigger Netlify" (#530)

--- apps/cockpit/src/app/login/[[...login]]/page.tsx
+++ apps/cockpit/src/app/login/[[...login]]/page.tsx
@@ -1,12 +1,7 @@
 import { LoginForm } from "@northware/ui/components/login";
 
 const Page = () => {
-  return (
-    <>
-      <h1>Hier hat sich etwas geändert</h1>
-      <LoginForm service="cockpit" />
-    </>
-  );
+  return <LoginForm service="cockpit" />;
 };
 
 export default Page;
