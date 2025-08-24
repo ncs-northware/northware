575a02500280c13101cef43e74ed816e8076a65a
Oke <56229919+onissen@users.noreply.github.com>
2025-08-24 14:34:25 +0000

    feat(cockpit): Filterable Permission UI
    Die einzelnen Permissions können jetzt per Search Input und Filter Buttons gefiltert werden. Das ermöglicht es dem User, die gewünschten Berechtigungen bessr zu finden und zu vergeben. Um diese Funktionalität zu erreichen, wurde dem CreateRoleForm, dem RolePermissionsForm und dem UpdateUserPermissionsForm eine entsprechende Funktionalität hinzugefügt.
    
    fix #539

--- apps/cockpit/src/app/admin/role/[recordId]/page.tsx
+++ apps/cockpit/src/app/admin/role/[recordId]/page.tsx
@@ -39,7 +39,7 @@ export default async function Page({
   return (
     <SidebarLayout
       breadcrumbs={[
-        { label: "Amin Panel", href: "/admin" },
+        { label: "Admin Panel", href: "/admin" },
         { label: "Rollenverwaltung", href: "/admin/role" },
         {
           label: details?.role.roleName || "Rolle",
