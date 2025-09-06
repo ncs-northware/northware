e6aad8a01656d55228f7cb0998f6e920e7d9452e
Oke <56229919+onissen@users.noreply.github.com>
2025-09-06 21:42:36 +0000

    fix(docs): Restructure Docs
    Die Inhalte der Dokumentation wurden in die neue Struktur überführt. Es gibt die Abschnitte Home, Apps und Packages unter denen die Artikel strukturiert sind.

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -4,30 +4,37 @@ import { fonts } from "@northware/ui/lib/fonts";
 import { DocsLayout } from "fumadocs-ui/layouts/notebook";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 import { RootProvider } from "fumadocs-ui/provider";
+import type { ReactNode } from "react";
 import { source } from "@/lib/source";
-import { ReactNode } from "react";
 
 const baseOptions: BaseLayoutProps = {
   githubUrl: "https://github.com/ncs-northware/northware",
-    nav: {
-      title: <Brand service="docs" />,
-      
-    },
-    // see https://fumadocs.dev/docs/ui/navigation/links
-    links: [],
-}
+  nav: {
+    title: <Brand service="docs" />,
+  },
+  // see https://fumadocs.dev/docs/ui/navigation/links
+  links: [],
+};
 
-export default function Layout({ children }: {readonly children: ReactNode}) {
+export default function Layout({ children }: { readonly children: ReactNode }) {
   return (
-    <html lang="de" suppressHydrationWarning className="theme-cockpit">
+    <html className="theme-cockpit" lang="de" suppressHydrationWarning>
       <body className={`flex min-h-screen flex-col ${fonts}`}>
         <RootProvider>
-          <DocsLayout tree={source.pageTree} {...baseOptions} sidebar={{ collapsible: false, tabs: [
-            {title: "General", url: "/"},
-            {title: "Apps", url: "/apps"},
-            {title: "Packages", url: "/packages"},
-          ]}}
-          tabMode="navbar" nav={{...baseOptions.nav, mode: "top"}}>
+          <DocsLayout
+            tree={source.pageTree}
+            {...baseOptions}
+            nav={{ ...baseOptions.nav, mode: "top" }}
+            sidebar={{
+              collapsible: false,
+              tabs: [
+                { title: "Home", url: "/" },
+                { title: "Apps", url: "/apps" },
+                { title: "Packages", url: "/packages" },
+              ],
+            }}
+            tabMode="navbar"
+          >
             {children}
           </DocsLayout>
         </RootProvider>
--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,10 +1,17 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  // biome-ignore lint/suspicious/useAwait: redirects is async
+  redirects: async () => {
+    return [
+      { source: "/apps", destination: "/apps/docs", permanent: true },
+      { source: "/packages", destination: "/packages/auth", permanent: true },
+    ];
+  },
 };
 
 export default withMDX(config);
