0e76aadd3294eabee63614e1247e9da15dba3d25
Oke <56229919+onissen@users.noreply.github.com>
2025-09-27 15:24:00 +0000

    chore(ui): fix styling of signin and profile
    Der Development-Banner und das Clerk Branding werden jetzt über die globals.css ausgeblendet, weil hierfür Clerks Internal Klassen angesprochen werden mussten. Der Nutzer kann sich nun auch wieder über die SignIn/Choose Komponente von allen Multisessions abmelden.

--- packages/ui/components/auth.tsx
+++ packages/ui/components/auth.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { SignIn, UserButton } from "@northware/auth/client";
 import type { ServiceType } from "@northware/service-config";
 import { Brand } from "@northware/ui/components/brand";
-import { LoaderCircleIcon } from "lucide-react";
+import { LoaderCircleIcon } from "@northware/ui/icons/lucide";
 
 export function SignInForm({ service }: { service: ServiceType }) {
   return (
@@ -13,7 +15,7 @@ export function SignInForm({ service }: { service: ServiceType }) {
             card: "bg-background text-card-foreground rounded-xl border shadow-sm",
             formFieldAction__password: "hidden",
             footerAction__havingTrouble: "hidden",
-            footer: "hidden",
+            // The Clerk Branding and Development Banner of the SignIn Component are hidden through the globals.css
           },
         }}
         fallback={<LoaderCircleIcon className="size-12 animate-spin" />}
@@ -36,8 +38,6 @@ export function UserMenu() {
         layout: { shimmer: false },
       }}
       showName
-      userProfileMode="navigation"
-      userProfileUrl="/profile"
     />
   );
 }
