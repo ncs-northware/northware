bca55e5b4e17c5fc1fee821df1bc3ac7b12ebeb9
Oke <56229919+onissen@users.noreply.github.com>
2025-10-26 16:54:05 +0000

    feat(cockpit): Add PermissionProvider and Metadata
    Die Seite /hr/management/employeeId ist jetzt nur noch für Benutzer mit der Berechtigung cockpit::hr-management.update sichtbar. Außerdem wird aus den Daten des Mitarbeiters der Titel der Seite generiert.

--- apps/cockpit/src/app/hr/management/[employeeId]/page.tsx
+++ apps/cockpit/src/app/hr/management/[employeeId]/page.tsx
@@ -1,8 +1,24 @@
 import { Headline } from "@northware/ui/components/headline";
+import { PermissionProvider } from "@northware/ui/components/permission-provider";
 import { notFound } from "next/navigation";
 import { EmployeePersonalForm } from "@/components/hr-forms";
-import { getEmployeePersonal } from "@/lib/hr-actions";
+import { getBasicEmployee, getEmployeePersonal } from "@/lib/hr-actions";
 import EmployeeSidebar from "./employee-sidebar";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ employeeId: number }>;
+}) {
+  const { employeeId } = await params;
+  const data = await getBasicEmployee(employeeId);
+  if (!data.success) {
+    return { title: "Mitarbeiter bearbeiten" };
+  }
+  return {
+    title: `${data.employee.employeeId} / ${data.employee.sirName}, ${data.employee.firstName}`,
+  };
+}
 
 export default async function Page({
   params,
@@ -18,11 +34,13 @@ export default async function Page({
 
   return (
     <EmployeeSidebar id={employeeId}>
-      <Headline level="h1">
-        {data.employee?.employeeId} / {data.employee?.sirName},{" "}
-        {data.employee?.firstName}
-      </Headline>
-      <EmployeePersonalForm data={data.employee} />
+      <PermissionProvider permissionKeys={["cockpit::hr-management.update"]}>
+        <Headline level="h1">
+          {data.employee?.employeeId} / {data.employee?.sirName},{" "}
+          {data.employee?.firstName}
+        </Headline>
+        <EmployeePersonalForm data={data.employee} />
+      </PermissionProvider>
     </EmployeeSidebar>
   );
 }
