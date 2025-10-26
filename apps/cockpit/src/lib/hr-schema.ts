670945ad9e640d856f304320b53b15d1390f56dd
Oke <56229919+onissen@users.noreply.github.com>
2025-10-26 16:41:20 +0000

    fix(cockpit): improove schema validation
    Die Schema Validation wurde so angepasst, das Pflichtfelder nicht als leere Strings gespeichert werden und alle Error-Messages wurden angepasst.

--- apps/cockpit/src/lib/hr-schema.ts
+++ apps/cockpit/src/lib/hr-schema.ts
@@ -2,16 +2,29 @@ import z from "zod";
 
 export const employeePersonalFormSchema = z.object({
   employeeId: z.number(),
-  sirName: z.string().max(75),
-  firstName: z.string().max(75),
-  sex: z.string(),
+  sirName: z
+    .string()
+    .min(1, { error: "Der Nachname ist ein Pflichtfeld" })
+    .max(75, { error: "Der Nachname darf maximal 75 Zeichen lang sein." }),
+  firstName: z
+    .string()
+    .min(1, { error: "Der Vorname ist ein Pflichtfeld" })
+    .max(75, { error: "Der Vorname darf maximal 75 Zeichen lang sein." }),
+  sex: z.string().min(1, { error: "Bitte wähle ein Geschlecht." }),
   birthday: z.date().nullable(),
-  street: z.string().max(100).nullable(),
+  street: z
+    .string()
+    .max(100, { error: "Die Straße darf maximal 100 Zeichen lang sein." })
+    .nullable(),
   zipcode: z.string().nullable(),
   city: z.string().nullable(),
-  meritalStatus: z.string(),
-  religion: z.string(),
-  taxClass: z.string(),
+  meritalStatus: z
+    .string()
+    .min(1, { error: "Bitte wähle einen Familienstand." }),
+  religion: z
+    .string()
+    .min(1, { error: "Bitte wähle eine Religionszugehörigkeit." }),
+  taxClass: z.string().min(1, { error: "Bitte wähle eine Steuerklasse." }),
   taxKids: z.number(),
 });
 
