777640d326ac6eeb31151592bd82a92118e1edd9
Oke <56229919+onissen@users.noreply.github.com>
2025-10-30 22:17:19 +0000

    fix(storybook): migrate main.ts to ESM-module

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,5 +1,9 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { createRequire } from "node:module";
 import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/nextjs";
+
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.
