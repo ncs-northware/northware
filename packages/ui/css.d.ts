f21dce7923287ae86ce0c33f4890a2c5a9a81844
Oke <56229919+onissen@users.noreply.github.com>
2026-08-01 19:02:41 +0000

    fix(ui): Add declaration file for CSS export
    The globals.css needs a type declaration in TS 6 to be treated correctly and not throw an error due to TS-Rule noUncheckedSideEffectImports.
    
    https://www.typescriptlang.org/tsconfig/#noUncheckedSideEffectImports

---
+++ packages/ui/css.d.ts
@@ -0,0 +1,6 @@
+/**
+ * This file declares a type for the CSS-files exported by this package. 
+ * The file is needed since TypeScript allowes no side-effect imports from "external" packages and throws an error when importing CSS-files in TypeScript. 
+**/
+
+declare module "*.css"
