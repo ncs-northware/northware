b8848cff782068dc3cfe3b40129a214619e8847c
Oke <56229919+onissen@users.noreply.github.com>
2026-08-28 14:06:29 +0000

    fix(ui): Fix implementation of InputGroupInput
    InputGroupInput had to be a component without children. This was a wrong implentation in the PasswordInput Component.

--- packages/ui/components/password-input.tsx
+++ packages/ui/components/password-input.tsx
@@ -8,36 +8,11 @@ import { cn } from "@northware/ui/lib/utils";
 import { EyeClosedIcon, EyeIcon } from "lucide-react";
 import { useState } from "react";
 
-// FIXME: Rethink Component with Radix's PasswordToggleField or InputGroups from shadcn
-
-// function OldPasswordInput({ ...props }) {
-//   const [showPassword, setShowPassword] = useState(false);
-
-//   return (
-//     <div className="flex">
-//       <Input
-//         className={cn("rounded-r-none border-r-0", props.className)}
-//         type={showPassword ? "text" : "password"}
-//         {...props}
-//       />
-//       <Button
-//         className="rounded-l-none border border-input bg-background dark:bg-input/30"
-//         onClick={() => setShowPassword(!showPassword)}
-//         size="icon"
-//         type="button"
-//         variant="ghost"
-//       >
-//         {showPassword ? <EyeClosedIcon /> : <EyeIcon />}
-//       </Button>
-//     </div>
-//   );
-// }
-
 export function PasswordInput({ ...props }) {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <InputGroup className={cn(props.className)} {...props}>
-      <InputGroupInput type={showPassword ? "text" : "password"}>
+      <InputGroupInput type={showPassword ? "text" : "password"} />
         <InputGroupAddon align="inline-end">
           <InputGroupButton
             aria-label="Passwort anzeigen/verbergen"
@@ -48,7 +23,6 @@ export function PasswordInput({ ...props }) {
             {showPassword ? <EyeClosedIcon /> : <EyeIcon />}
           </InputGroupButton>
         </InputGroupAddon>
-      </InputGroupInput>
     </InputGroup>
   );
 }
